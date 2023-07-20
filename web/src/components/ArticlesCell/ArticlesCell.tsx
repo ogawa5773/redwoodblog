bb17a6364085bd6b7156ce9c862bdb58bbc90433
daichi <anaminkumamoto@gmail.com>
2023-07-20 22:58:39 +0900

    Initial commit

---
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -0,0 +1,33 @@
+import type { ArticlesQuery } from 'types/graphql'
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+import Article from 'src/components/Article'
+
+// query ArticlesQueryでpostsを叩けばいいと判別できてる仕組みは
+export const QUERY = gql`
+  query ArticlesQuery {
+    articles: posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
+  return (
+    <>
+      {articles.map((article) => (
+        <Article key={article.id} article={article} />
+      ))}
+    </>
+  )
+}
